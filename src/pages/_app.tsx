178e2eb8d5d837b1f0b195a4487054bf228d31b1
i am Chuppyy <62337417+chuppyy@users.noreply.github.com>
2024-03-03 21:29:31 +0700

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,41 @@
 import Head from "next/head";
 import App, { AppProps, AppContext } from "next/app";
 import Layout from "../layouts/layout";
-import '../globals.css'
+import "../globals.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){
+                w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5LPK24FH');
+            `,
+          }}
+        />
       </Head>
 
       <Layout>
         <Component {...pageProps} />
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5LPK24FH"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
       </Layout>
     </>
   );
